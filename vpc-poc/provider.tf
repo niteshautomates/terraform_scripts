b03ca79e74bfaa6ed887fdc2da5589c09bdf88be
Nitesh Wayafalkar <niteshwayafalkar@gmail.com>
2025-07-04 00:46:37 +0530

    Update provider.tf

--- vpc-poc/provider.tf
+++ vpc-poc/provider.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+
+    }
+    random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
+    }
+  }
+  cloud {
+    organization = "NiteshAutomates"
+    workspaces {
+      name = "terraform_scripts"
+    }
+  }
+}
+
 provider "aws" {
-    region = var.region_name
+  region = "ap-south-1"
 }
